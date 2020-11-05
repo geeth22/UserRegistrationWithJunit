afc926efbf1a730f5f6208032adfd9164bc05b80
geeth22 <geethchandra22@gmail.com>
2020-11-05 15:19:52 +0530

    enter first name starts with cap and has minimum 3 characters

--- TestingUserRegProblem.java
+++ TestingUserRegProblem.java
@@ -1,2 +1,18 @@
-/*   Welcome to User Registration Validation with Junit  */
+import org.junit.Assert;
+import org.junit.Test;
 
+public class TestingUserRegProblem {
+    @Test
+    public void testingFirstName() {
+        UserRegistration userRegistration = new UserRegistration();
+        boolean isFNameValid = userRegistration.checkFName("Geeth");
+        Assert.assertTrue(isFNameValid);
+    }
+
+    @Test
+    public void testFirstNameInvalidMustReturnFalse() {
+        UserRegistration userRegistration = new UserRegistration();
+        boolean isFnameValid = userRegistration.checkFName("geeth");
+        Assert.assertFalse(isFnameValid);
+    }
+}
